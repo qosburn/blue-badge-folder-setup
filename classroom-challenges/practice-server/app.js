bc3409a0629a8795cb092668901e873995b74e8f
qosburn <qosburn@mac.com>
2021-07-29 19:20:04 -0400

    versioon2

--- classroom-challenges/practice-server/app.js
+++ classroom-challenges/practice-server/app.js
@@ -1,11 +1,14 @@
 let express = require('express');
 const nodemon = require('nodemon');
 let app = express();
+var testController = require('./controllers/testcontroller');
+var calculatorController = require('./controllers/calculatorController');
 // req an res request and response NOTE that you can be specific on the app.post /app.delete
-app.use('/test', function (req, res) {
-  let response = { message: 'This is a test' };
-  res.json(response);
-});
-app.listen(3001, function () {
-  console.log('app is listening on port 3001');
+app.use(express.json());
+
+app.use('/test', testController);
+app.use('/calculator', calculatorController);
+
+app.listen(3000, function () {
+  console.log('app is listening on port 3000');
 });
