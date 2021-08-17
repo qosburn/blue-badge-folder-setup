3b4fb4106d429dafaf49a2c2e074f5e42ce770e2
qosburn <qosburn@mac.com>
2021-08-17 17:48:52 -0400

    updated day3

---
+++ react-classroom/react-lessons/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+
+function App() {
+  // This is a comment
+  const welcomeName = 'Rob!!!';
+
+  return (
+    <div className="App">
+      <h1>Welecome to React, {welcomeName}</h1>
+    </div>
+  );
+}
+
+export default App;
